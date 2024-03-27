ff7b926aa1ff4988964e2501519bf98720938f3e
Your Name <lokeshtiwari212@gmail.com>
2024-03-27 07:34:04 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Statusbar from './layout/Statusbar';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="App">
+      <Statusbar />
+    </div>
   );
 }
 
---
+++ src/layout/Statusbar.js
@@ -0,0 +1,27 @@
+// Statusbar.js
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+function Statusbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+          </Nav>
+            <Button variant="outline-success">Search</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Statusbar;
