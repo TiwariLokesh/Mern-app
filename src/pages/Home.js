7f6c607d84f5fba7cb6b1476e95619173a2e3a0c
Your Name <lokeshtiwari212@gmail.com>
2024-03-27 21:46:41 +0530

    called the component in home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import Statusbar from "../layout/Statusbar";
 import HomeCards from '../components/HomeCards'
 import Footer from '../layout/Footer'
+import Hero from "../components/Hero";
 function Home() {
   return (
     <>
       <Statusbar />
+      <Hero/>
       <HomeCards/>
       <Footer/>
     </>
