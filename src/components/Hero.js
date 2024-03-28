3817d9da08062ffc870c0ac70c1e461d1844fe4a
Your Name <lokeshtiwari212@gmail.com>
2024-03-28 10:22:22 +0530

    updated hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,33 +1,40 @@
+import { Button, Form } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 
 function Hero() {
   return (
     <Carousel data-bs-theme="dark">
+        <Carousel.Caption style={{zIndex:"10"}}>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success text-white bg-success">Search</Button>
+          </Form>
+        </Carousel.Caption>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{filter:"brightness(30%)"}}
           src="https://source.unsplash.com/random/900×700/?Restaurant"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{filter:"brightness(30%)"}}
           src="https://source.unsplash.com/random/900×700/?Burger"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{filter:"brightness(30%)"}}
           src="https://source.unsplash.com/random/900×700/?Foods"
           alt="Third slide"
         />
